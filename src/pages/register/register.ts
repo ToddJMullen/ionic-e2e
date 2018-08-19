e506dac92dafb6b03ae314e911d77dde48d69760
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-19 18:03:02 -0400

    Implement Registration with redirect

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase/app";
+
+import { HomePage } from "../home/home";//needed for register/login exit nav
 
 /**
  * Generated class for the RegisterPage page.
@@ -15,11 +19,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  registration = {
+    email: ""
+    ,pwd1: ""
+    ,pwd2: ""
+  };
+
+  constructor(
+    public navCtrl: NavController
+    ,public navParams: NavParams
+    ,public alertCtrl: AlertController
+    ,public ngFireAuth: AngularFireAuth
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  displayAlert( alertTitle:string, alertSub:string ){
+    let theAlert = this.alertCtrl.create({
+      title: alertTitle
+      ,subTitle: alertSub
+      ,buttons: ["OK"]
+    });
+    theAlert.present();
+  }
+
+
+  registerAccount(){
+    if( this.registration.pwd1 != this.registration.pwd2 ){
+      this.displayAlert("Password Error!", "The entered passwords do not match!");
+      this.registration.pwd1 = "";
+      this.registration.pwd2 = "";
+      return;
+    }
+    // if( !this.registration.email ) {
+    //   this.displayAlert("Missing Email!", "Please enter a valid email.");
+    //   return;
+    // }
+    this.ngFireAuth.auth.createUserWithEmailAndPassword( this.registration.email, this.registration.pwd1 )
+    .then( rsp => this.registerSuccess(rsp) )
+    .catch( err => this.displayAlert("Registration Error", err ) )
+    ;
+  }
+
+  registerSuccess( rsp ){
+    this.displayAlert( `Welcome ${rsp.email}`
+      ,`You have been successfully registered and automatically logged in.`
+    );
+    this.ngFireAuth.auth.signInWithEmailAndPassword( this.registration.email, this.registration.pwd1 )
+    .then( rsp => this.navCtrl.push(HomePage) )
+    .catch( err => this.displayAlert( "Login Error", `There has been a problem logging you in.`) )
+    ;
   }
 
 }
