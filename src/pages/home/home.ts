38de9fc5349c0a3686e6a0ba456802bdabaca9af
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-19 00:12:06 -0400

    HomePage, Imlement first draft of initial page / menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  menuData = [
+    {title:"Our Menu",pic:"assets/img/soup1.jpg", pushPage:"MenuPage"}
+    ,{title:"Account",pic:"assets/img/coffee-people3.jpg", pushPage:"AccountPage"}
+    ,{title:"Locations",pic:"assets/img/cafe2.jpg", pushPage:"LocationsPage"}
+    ,{title:"About",pic:"assets/img/coffee6.jpg", pushPage:"AboutPage"}
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
