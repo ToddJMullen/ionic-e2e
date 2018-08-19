76e584366f6114cae9196abae39a2c4c53d3adaf
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-19 01:10:23 -0400

    Generate base Login page & module

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,10 +8,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   menuData = [
-    {title:"Our Menu",pic:"assets/img/soup1.jpg", pushPage:"MenuPage"}
-    ,{title:"Account",pic:"assets/img/coffee-people3.jpg", pushPage:"AccountPage"}
-    ,{title:"Locations",pic:"assets/img/cafe2.jpg", pushPage:"LocationsPage"}
-    ,{title:"About",pic:"assets/img/coffee6.jpg", pushPage:"AboutPage"}
+    {title:"Our Menu"   ,pic:"assets/img/soup1.jpg", pushPage:"MenuPage"}
+    ,{title:"Account"   ,pic:"assets/img/coffee-people3.jpg", pushPage:"AccountPage"}
+    ,{title:"Locations" ,pic:"assets/img/cafe2.jpg", pushPage:"LocationsPage"}
+    ,{title:"About"     ,pic:"assets/img/coffee6.jpg", pushPage:"AboutPage"}
   ]
 
   constructor(public navCtrl: NavController) {
