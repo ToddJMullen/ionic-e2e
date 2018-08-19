45a51f3be83ff4d0420f77c24b0569003026b9af
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-19 01:09:15 -0400

    Setup Firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from  "angularfire2/auth";
+
+import { FIREBASE_ID, FIREBASE_KEY, FIREBASE_SENDER_ID } from "./Keys";
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+export const  firebaseConfig = {
+  apiKey: FIREBASE_KEY
+  ,authDomain: `${FIREBASE_ID}.firebaseapp.com`
+  ,databaseURL: `https://${FIREBASE_ID}.firebaseio.com`
+  ,storageBuckt: `${FIREBASE_ID}.appspot.com`
+  ,messagingSenderId: FIREBASE_SENDER_ID
+}
 
 @NgModule({
   declarations: [
@@ -15,6 +29,9 @@ import { HomePage } from '../pages/home/home';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
+    ,AngularFireModule.initializeApp(firebaseConfig)
+    ,AngularFireDatabaseModule
+    ,AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
