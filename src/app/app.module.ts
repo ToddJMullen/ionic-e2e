0fff27d35ed9ec323d71907f10d797669b116ee0
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-02-16 14:12:50 -0500

    Add/install Paypal module/sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 import { AngularFireAuthModule } from  "angularfire2/auth";
 
+import { PayPal } from "@ionic-native/paypal";
 
-import { FIREBASE_ID, FIREBASE_KEY, FIREBASE_SENDER_ID } from "./Keys";
+
+import { FIREBASE_ID, FIREBASE_KEY, FIREBASE_SENDER_ID, PAYPAL_SANDBOX_CLIENT_ID } from "./Keys";
 //services
 // import { FCM } from "@ionic-native/fcm";
 // ^^^ commented out due to dependency not supported by ionic serve
@@ -70,6 +72,7 @@ export const  firebaseConfig = {
     ,RewardServiceProvider,
     MenuServiceProvider,
     CartServiceProvider
+    ,PayPal
     // ,FCM// commented out due to dependency not supported by ionic serve
   ]
 })
